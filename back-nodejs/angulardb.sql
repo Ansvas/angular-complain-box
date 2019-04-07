8586cdb3487f449795537685b9dd4dcf2f0b2e04
Ansvas <anshul60198@gmail.com>
2019-04-07 21:36:20 +0530

    first backend commit

---
+++ back-nodejs/angulardb.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 07, 2019 at 05:39 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `angulardb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comm_id` int(11) NOT NULL,
+  `comp_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comm_id`, `comp_id`, `user_id`, `comment`) VALUES
+(1, 1, 1, 'my phone is not working'),
+(2, 1, 1, 'my phone is not working(duplicate)'),
+(3, 1, 1, 'my phone is not working(duplicate)'),
+(4, 6, 5, 'new comment'),
+(5, 6, 5, 'new comment');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaints`
+--
+
+CREATE TABLE `complaints` (
+  `comp_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `body` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaints`
+--
+
+INSERT INTO `complaints` (`comp_id`, `user_id`, `title`, `body`) VALUES
+(1, 5, 'this is tiltle', 'body here');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `password`) VALUES
+(1, 'anshul@gmail.com', 'anshul'),
+(2, 'anshul1@gmail.com', 'anshul1'),
+(3, 'anshul2@gmail.com', 'anshul2'),
+(4, 'anshul3@gmail.com', 'anshul3'),
+(5, 'anshul6@gmail.com', 'anshul6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comm_id`);
+
+--
+-- Indexes for table `complaints`
+--
+ALTER TABLE `complaints`
+  ADD PRIMARY KEY (`comp_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `complaints`
+--
+ALTER TABLE `complaints`
+  MODIFY `comp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ back-nodejs/index.js
@@ -0,0 +1,193 @@
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+var jsondata = require('./data.json');
+var _und = require('underscore');
+const mysql = require('mysql');
+const morgan = require('morgan');
+
+app.use(bodyparser.urlencoded({ extended: true}));
+app.use(bodyparser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+const connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		database: 'angulardb'
+	})
+
+router.post('/auth',function(req,res)
+{
+	console.log(req.body);
+	var query= 'SELECT * FROM users where email = "'+req.body.email+'" and password = "'+req.body.password+'"';
+	console.log(query);
+	connection.query(query,(err,rows,fields) => {
+		console.log('we fetched users successfully');
+		if(rows.length>0)
+		{
+			res.json(rows);
+		}
+		else
+		{
+			res.json('null');
+		}
+	})
+})
+
+
+router.post('/register',function(req,res)
+{
+	console.log(req.body);
+	if(req.body.email && req.body.password)
+	var query= 'INSERT INTO users (email,password) VALUES ("'+req.body.email+'","'+req.body.password+'")';
+	else
+		res.json('error: parameters are not correct');
+	console.log(query);
+	connection.query(query,(err,rows,fields) => {
+		if(err)
+			res.json('err:'+err);
+		if(rows)
+		{
+			res.json('success');
+		}
+		if(fields)
+		{
+			res.json('fields:'+fields);
+		}
+	})
+})
+
+router.post('/comment',function(req,res)
+{
+	console.log(req.body);
+	if(req.body.comp_id && req.body.user_id && req.body.comment)
+	var query= 'INSERT INTO comments (comp_id,user_id,comment) VALUES ('+req.body.comp_id+','+req.body.user_id+',"'+req.body.comment+'")';
+	else
+	res.json('error: params are not correct');
+	console.log(query);
+	connection.query(query,(err,rows,fields) => {
+		if(err)
+			res.json('err:'+err);
+		if(rows)
+		{
+			res.json('success');
+		}
+		if(fields)
+		{
+			res.json('fields:'+fields);
+		}
+	})
+})
+
+router.get('/comment',function(req,res)
+{
+	var count=0;
+	/*build query*/
+	var query= 'SELECT * FROM comments';
+	if(req.body.user_id || req.body.comm_id || req.body.comp_id)
+	{
+		console.log('in main if');
+		query = query+' where ';
+		if(req.body.user_id)
+		{
+			console.log('in user id');
+			query = query+' user_id = '+req.body.user_id;
+			count++;
+		}
+
+
+		if(req.body.comm_id)
+		{
+			console.log('in comment');
+			if(count>0)
+				query = query+' and comm_id = '+req.body.comm_id;
+			else
+				query =query+' comm_id = '+req.body.comm_id;
+			count++;
+		}
+
+		if(req.body.comp_id)
+		{
+			console.log('in complain');
+			if(count>0)
+				query = query+' and comp_id = '+req.body.comp_id;
+			else
+				query = query+' comp_id = '+req.body.comp_id;
+		}
+	}
+	console.log(query);
+	connection.query(query,(err,rows,fields) => {
+		if(err)
+			res.json('err:'+err);
+		if(rows)
+			res.json(rows);		
+		/*if(fields)		
+			res.json('fields:'+fields);*/
+		
+	})
+})
+
+router.post('/complain',function(req,res)
+{
+	console.log(req.body);
+	if(req.body.user_id && req.body.title && req.body.body);
+	var query= 'INSERT INTO complaints (user_id,title,body) VALUES ('+req.body.user_id+',"'+req.body.title+'","'+req.body.body+'")';
+	console.log(query);
+	connection.query(query,(err,rows,fields) => {
+		if(err)
+			res.json('err:'+err);
+		if(rows)
+		{
+			res.json('success');
+		}
+		if(fields)
+		{
+			res.json('fields:'+fields);
+		}
+	})
+})
+
+router.get('/complain',function(req,res)
+{
+	var count=0;
+
+	/*build query*/
+	var query= 'SELECT * FROM complaints';
+	if(req.body.user_id || req.body.comp_id)
+	{
+		query = query+' where ';
+		if(req.body.user_id)
+		{
+			console.log('in user id');
+			query = query+' user_id = '+req.body.user_id;
+			count++;
+		}
+
+		if(req.body.comp_id)
+		{
+			console.log('in comment');
+			if(count>0)
+				query = query+' and comp_id = '+req.body.comp_id;
+			else
+				query =query+' comp_id = '+req.body.comp_id;
+			count++;
+		}
+	}
+	console.log(query);
+	connection.query(query,(err,rows,fields) => {
+		if(err)
+			console.log('err:'+err);
+		if(rows)
+			res.json(rows);
+		
+		if(fields)		
+			console.log('fields:'+fields);
+		
+	})
+})
+
+app.use('/api',router);
+app.listen(port);
